6069a0768c166dd40cea47bcd92d034a6438d2f0
6pod9 <zjc8966@me.com>
2021-07-16 22:26:44 +0800

    NavLogo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>What's up</h1>
-    </div>
+    <Router>
+      <Navbar/>
+    </Router>
   );
 }
 
