ad8213f9f35eaba05f23f34714a16496df23e866
6pod9 <zjc8966@me.com>
2021-07-17 11:33:01 +0800

    NavBar 1.0

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,17 +1,37 @@
 import React from 'react'
-import { Nav, NavbarContainer, NavLogo } from './NavbarElements'
+import { MobileIcon, Nav, NavbarContainer, NavBtn, NavBtnLink, NavItem, NavLinks, NavLogo, NavMenu } from './NavbarElements'
+import { FaBars } from 'react-icons/fa'
 
 const Navbar = () => {
-    return (
-        <>
-        <Nav>
-            <NavbarContainer>
-                <NavLogo to='/'>b_d</NavLogo>
-                <h1>hey</h1>
-            </NavbarContainer>
-        </Nav>
-        </>
-    )
+  return (
+  <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to='/'>b_d</NavLogo>
+          <MobileIcon>
+            <FaBars/>
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to='about'>About</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to='discover'>Discover</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to='services'>Services</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to='signup'>Sign Up</NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+          </NavBtn>
+        </NavbarContainer>
+      </Nav>
+    </>
+  )
 }
 
 export default Navbar
